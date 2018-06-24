aa5a0eface55f5e1bd5434c3adaccde68107191d
GalAster <galaster@foxmail.com>
2018-06-24 13:44:58 +0800

    研究记录::获取Scp International 各分部的信息

---
+++ Packages/ScpInternation.m
@@ -0,0 +1,81 @@
+ScpInternation::usage = "";
+Begin["`Internation`"];
+
+Options[ScpInternation] = {Normal -> True};
+ScpInternation[OptionsPattern[]] := Block[
+    {
+        all = Quiet@Join[ScpGetCN[], ScpGetOther[]],
+        dQ = Or[
+            #["Name"] === "[禁止访问]",
+            !StringContainsQ[#["Number"], "-" ~~ __ ~~ "-"],
+            !NumberQ[ToExpression@StringTake[#["Number"], -1]]
+        ]&;
+    },
+    If[OptionValue[Normal],
+        SortBy[Join[DeleteCases[all, _?dQ], $ScpAdd], #["Number"]&],
+        {Select[all, StringTake[#["Number"], -1] === "J"&], Select[all, dQ]}
+    ]
+];
+
+
+
+
+
+ScpGetCN[] := Block[
+    {
+        link = "//scp-wiki-cn.wikidot.com/scp-international",
+        site, case1
+    },
+    link = "//scp-wiki-cn.wikidot.com/scp-series-cn";
+    site = URLExecute[link, {"HTML", "XMLObject"}];
+    case1 = Cases[site,
+        XMLElement["li", {},
+            {XMLElement["a", {"shape" -> "rect", "href" -> url_}, {num_}], name_}
+        ] :> <|
+            "Url" -> StringJoin["//scp-wiki-cn.wikidot.com", url],
+            "Number" -> ScpNationNumber[num],
+            "Name" -> StringDelete[name, " - "]
+        |>,
+        Infinity
+    ];
+    case1
+];
+ScpNationNumber[input_] := Block[
+    {sp = StringSplit[input, "-"]},
+    If[And[Length@sp == 3, NumberQ[ToExpression@sp[[2]]]], "SCP-" <> sp[[3]] <> "-" <> sp[[2]], input]
+];
+ScpGetOther[] := Block[
+    {
+        link = "//scp-wiki-cn.wikidot.com/scp-international",
+        site, case1, case2
+    },
+    site = URLExecute[link, {"HTML", "XMLObject"}];
+    case1 = Cases[site,
+        XMLElement["li", {},
+            {XMLElement["a", {"shape" -> "rect", "href" -> url_}, {num_}], name_}
+        ] :> <|
+            "Url" -> StringJoin["//scp-wiki-cn.wikidot.com", url],
+            "Number" -> ScpNationNumber[num],
+            "Name" -> StringDelete[name, " - "]
+        |>,
+        Infinity
+    ];
+    case2 = SequenceCases[
+        Cases[site,
+            XMLElement["p", {}, {x__}] :> x, Infinity], {
+            XMLElement["a", {__, "href" -> url_}, {num_}], name_, XMLElement["br", ___]
+        } :> <|
+            "Url" -> StringJoin["//scp-wiki-cn.wikidot.com", url],
+            "Number" -> ScpNationNumber[num],
+            "Name" -> StringDelete[name, " - "]
+        |>];
+    Join[case1, case2]
+];
+$ScpAdd = {
+    <|"Url" -> "//scp-wiki-cn.wikidot.com/scp-cn-066", "Number" -> "SCP-CN-066", "Name" -> "居然对八旬老人做出这种不知羞耻的..."|>,
+    <|"Url" -> "//scp-wiki-cn.wikidot.com/scp-cn-296", "Number" -> "SCP-CN-296", "Name" -> "艾斯 瑟 癖 横杠 瑟 恩 横杠 二九六"|>
+};
+
+
+
+End[]
--- Packages/ScpSpecial.m
+++ Packages/ScpSpecial.m
@@ -6,7 +6,29 @@ Begin["`Special`"];
 
 
 (* ::Section:: *)
-(*ScpD*)
+(*Scp-J*)
+
+
+$ScpCaseJ = {
+    "//scp-wiki-cn.wikidot.com/joke-scps",
+    "//scp-wiki-cn.wikidot.com/joke-scps-cn"
+};
+ScpFromJ[link_] := Cases[
+    URLExecute[link, {"HTML", "XMLObject"}],
+    XMLElement["li", {},
+        {XMLElement["a", {"shape" -> "rect", "href" -> url_}, {num_}], name_}
+    ] :> <|
+        "Url" -> StringJoin["//scp-wiki-cn.wikidot.com", url],
+        "Number" -> num,
+        "Name" -> StringDelete[name, " - "]
+    |>,
+    Infinity
+];
+
+
+
+(* ::Section:: *)
+(*Scp-D*)
 
 
 $ScpCaseD = {
@@ -31,27 +53,7 @@ ScpFromD[link_] := Cases[FirstCase[
 
 
 (* ::Section:: *)
-(*ScpArc*)
-
-
-$ScpCaseArc = {
-    "//scp-wiki-cn.wikidot.com/archived-scps"
-};
-ScpFromArc[link_] := Cases[
-    URLExecute[link, {"HTML", "XMLObject"}],
-    XMLElement["li", {},
-        {XMLElement["a", {"shape" -> "rect", "href" -> url_}, {num_}], name_}
-    ] :> <|
-        "Url" -> StringJoin["//scp-wiki-cn.wikidot.com", url],
-        "Number" -> num,
-        "Name" -> StringDelete[name, " - "]
-    |>,
-    Infinity
-];
-
-
-(* ::Section:: *)
-(*ScpEx*)
+(*Scp-Ex*)
 
 
 $ScpCaseEx = {
@@ -71,15 +73,13 @@ ScpFromEx[link_] := Cases[
 
 
 (* ::Section:: *)
-(*ScpJ*)
+(*Scp-Arc*)
 
 
-$ScpCaseJ = {
-    "//scp-wiki-cn.wikidot.com/joke-scps",
-    "//scp-wiki-cn.wikidot.com/joke-scps-cn"
+$ScpCaseArc = {
+    "//scp-wiki-cn.wikidot.com/archived-scps"
 };
-$ScpCaseSp = {};
-ScpFromJ[link_] := Cases[
+ScpFromArc[link_] := Cases[
     URLExecute[link, {"HTML", "XMLObject"}],
     XMLElement["li", {},
         {XMLElement["a", {"shape" -> "rect", "href" -> url_}, {num_}], name_}
@@ -91,6 +91,24 @@ ScpFromJ[link_] := Cases[
     Infinity
 ];
 
+
+(* ::CodeText:: *)
+(*all = Quiet@Join[ScpGetCN[], ScpGetOther[], $ScpAdd]*)
+(*Select[all, StringTake[#["Number"], -1] === "J" &]*)
+
+$ScpCaseAdd = {
+    <|"Url"->"//scp-wiki-cn.wikidot.com/scp-one-half-jp-j","Number"->"SCP-1/2-JP-J","Name"->"\:534a\:4ef7"|>,
+    <|"Url"->"//scp-wiki-cn.wikidot.com/scp-36-6-jp-j","Number"->"SCP-36.6-JP-J","Name"->"\:4e0d\:53ef\:89c1\:7684\:6076\:9b54"|>,
+    <|"Url"->"//scp-wiki-cn.wikidot.com/scp-666-jp-j","Number"->"SCP-666-JP-J","Name"->"\:7a76\:6781\:6050\:6016\:7269\:4f53"|>,
+    <|"Url"->"//scp-wiki-cn.wikidot.com/scp-710-jp-j","Number"->"SCP-710-JP-J","Name"->"\:57fa\:91d1\:4f1a\:795e\:62f3"|>,
+    <|"Url"->"//scp-wiki-cn.wikidot.com/scpaaaaaaaaaaaaaaaaaa-jp-j","Number"->"SCP-\:54e6\:5594\:5594\:963f\:963f\:963f\:963f\:963f\:963f\:963f\:963f-JP-J","Name"->""|>,
+    <|"Url"->"//scp-wiki-cn.wikidot.com/scp-017-fr-j","Number"->"SCP-017-FR-J","Name"->"\:9752\:9752\:9152\:539f"|>,
+    <|"Url"->"//scp-wiki-cn.wikidot.com/scp-008-de-j","Number"->"SCP-008-DE-J","Name"->"\:7fd4\:ff1f\:ff01"|>
+};
+
+
+(* ::Chapter::Closed:: *)
+(*End*)
 
 
 End[]
